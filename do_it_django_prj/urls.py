6e163a0983937a330ba874ce2ac0f02fe5907f4e
BongCheon Kim <kbc6710@gmail.com>
2022-09-25 21:14:58 +0900

    Django 프로젝트 생성

---
+++ do_it_django_prj/urls.py
@@ -0,0 +1,9 @@
+"""
+이곳은 사용자가 URL에 접속하였을때 어떻게 웹 사이트를 작동시킬지를 정리해 놓은 파일입니다
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
