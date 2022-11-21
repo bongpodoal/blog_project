b7a8e42bc13acd7e403277f4f480c5a0afe8b0d5
BongCheon Kim <kbc6710@gmail.com>
2022-11-21 12:55:51 +0900

    네비게이션바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,23 @@ from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Web')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(logo_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(logo_btn.attrs['href'], '/about_me')
 
     def test_post_list(self):
 
@@ -17,12 +34,6 @@ class TestView(TestCase):
         # 1.3 페이지 타이틀은 ‘Blog’이다.
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        # 1.4 내비게이션 바가 있다.
-        navbar = soup.nav
-        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
-
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
 
@@ -51,4 +62,25 @@ class TestView(TestCase):
 
         # 3.4‘아직 게시물이 없습니다’라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+        def test_post_detail(self):
+            post_000 = Post.objects.create(
+                title='첫 번째 포스트입니다.',
+                content='Hello World. We are the world.',
+            )
+            self.assertEqual(post_000.get_absolute_url(), '/blog/1/')
+            response = self.client.get(post_000.get_absolute_url())
+            self.assertEqual(response.status_code, 200)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            self.navbar_test(soup)
+
+            self.assertIn(post_001.title, soup.title.text)
+
+            main_area = soup.find('div', id='main-area')
+            post_area = main_area.find('div', id='post-area')
+            self.assertIn(post_001.tutle, post_area.text)
+
+            self.assertIn(post_001.content, post_area.text)
+
+
+
 
