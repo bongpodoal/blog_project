56db945a1d504a8da1a167836f12d3f0a17a118a
BongCheon Kim <kbc6710@gmail.com>
2022-11-24 11:10:33 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/models.py
+++ blog/models.py
@@ -55,5 +55,15 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
     # 포스트의 제목과 번호를 문자열로 표현
     # author: 추후 추가 예정
