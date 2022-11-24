3c2e62eff2db6d0410b4a54765233b2fe5bcd748
BongCheon Kim <kbc6710@gmail.com>
2022-11-24 14:54:00 +0900

    대문 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
+
     return render(
         request,
-        'single_pages/landing.html'
+        'single_pages/landing.html',
+        {
+            'recent_posts': recent_posts,
+        }
     )
-# 도메인 뒤에 아무것도 없을 때 landing.html을 불러옴
+
+
 def about_me(request):
     return render(
         request,
         'single_pages/about_me.html'
     )
-
-# 도메인 뒤에 about_me가 있을 때 about_me.html을 불러옴
