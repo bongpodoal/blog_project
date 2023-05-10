befa8b787b208af6d8ca4a764040a4779196ca6d
unknown <kbc6710@gmail.com>
2023-05-10 10:45:04 +0900

    좀 더 친절하게 한글로 바꿈

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('profiles/<user_id>/', views.profile, name='profile'),
-   # path('profiles/create/', ProfileCreateView.as_view(), name='create_profile'),
+    # path('profiles/create/', ProfileCreateView.as_view(), name='create_profile'),
     path('profile/<int:pk>/update/', ProfileUpdateView.as_view(), name='profile_update'),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
