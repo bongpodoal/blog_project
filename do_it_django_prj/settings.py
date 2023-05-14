b64c46ebaf1317397d33b06fcad06ce25fd65e96
unknown <kbc6710@gmail.com>
2023-05-14 23:07:18 +0900

    베타테스트

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'do_it_django_prj.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 USE_L10N = True
