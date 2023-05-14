a6081cd4e47d43a240f059f10cb71eb8751e47a1
unknown <kbc6710@gmail.com>
2023-05-15 03:26:37 +0900

    백업본

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
