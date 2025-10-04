2815aaa1e437bd5555859c15e85d29fc6de3f2c2
bongpodoal <76566179+bongpodoal@users.noreply.github.com>
2025-10-04 17:47:07 +0900

    페이지추가

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,5 +3,10 @@ from . import views
 urlpatterns = [
     path('about_me/', views.about_me),
     path('', views.landing),
+
+    path('eunjun/', views.eunjun, name='eunjun'),
+    path('siyoon/', views.siyoon, name='siyoon'),
+    path('jaehyeong/', views.jaehyeong, name='jaehyeong'),
+
 ]
 # 도메인 뒤에 아무것도 없을 때는 landing 함수를, 뒤에 about_me/ 가 붙었을 경우에는 about_me 함수를 실행한다.
--- single_pages/views.py
+++ single_pages/views.py
@@ -19,3 +19,13 @@ def about_me(request):
         request,
         'single_pages/about_me.html'
     )
+
+
+def eunjun(request):
+    return render(request, 'single_pages/eunjun.html')
+
+def siyoon(request):
+    return render(request, 'single_pages/siyoon.html')
+
+def jaehyeong(request):
+    return render(request, 'single_pages/jaehyeong.html')
